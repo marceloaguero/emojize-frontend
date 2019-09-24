e570a8a8e4c89ab7a5f1ab36c66081ae971e2c43
Marcelo Aguero <marceloaguero@gmail.com>
2019-09-24 17:59:35 -0300

    Change protobuf path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const {EmojiRequest, EmojiResponse} = require('./emoji_pb.js');
-const {EmojiServiceClient} = require('./emoji_grpc_web_pb.js');
+const {EmojiRequest, EmojiResponse} = require('./proto/emoji_pb.js');
+const {EmojiServiceClient} = require('./proto/emoji_grpc_web_pb.js');
 
 var client = new EmojiServiceClient('http://' + window.location.host);
 var editor = document.getElementById('editor');
